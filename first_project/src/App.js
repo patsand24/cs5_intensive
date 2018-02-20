c59fb156a011cad7982d4d0950572877b61a956c
Patrick Sandoval <patsand24csu@yahoo.com>
2018-02-20 09:06:21 -0800

    first commit

---
+++ first_project/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Functional from './components/Functional';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Functional />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ first_project/src/components/Functional.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Functional = () => {
+    return(
+        <div>This is a functional Component</div>
+    )
+};
+
+export default Functional;
